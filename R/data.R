3dce4fd32afd4f7751dbf0d6274b862a019acac8
itamarcaspi <caspi.itamar@gmail.com>
2018-09-19 22:33:09 +0200

    changed data type

--- R/data.R
+++ R/data.R
@@ -3,11 +3,11 @@
 #' S&P 500 price to dividend ratio: January 1973-- July 2017
 #'
 #'
-#' @format Time series data
+#' @format Data frame
 #' @source Datastream International.
 #' @keywords datasets
 #' @examples
-#' plot(snp)
+#' head(snp)
 #'
 "snp"
 
@@ -17,11 +17,11 @@
 #' an index constructed as a GDP weighted 10-year government bond yield of the
 #' GIIPS (Greece, Ireland, Italy, Portugal, and Spain) countries.
 #'
-#' @format Time series data
+#' @format Data frame
 #' @source Datastream International.
 #' @keywords datasets
 #' @examples
-#' plot(spread)
+#' head(spread)
 #'
 "spread"
 
